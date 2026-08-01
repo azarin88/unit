ab9db5b80ff8a4123a5be2efbdbdc76b23df5cb0
Azarin Maxim <azarin.m88@gmail.com>
2026-08-01 12:10:17 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldRemainWithAmountOf999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 1;
+        int actual = cashbackHackService.remain(999);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldRemainWithAmountOf1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldRemainWithAmountOf1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = 999;
+        int actual = cashbackHackService.remain(1001);
+        Assertions.assertEquals(expected, actual);
+    }
+}
