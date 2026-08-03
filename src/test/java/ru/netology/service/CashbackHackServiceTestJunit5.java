bd5a6e544020ee240b492b78e7ed8e7e4db319a5
Azarin Maxim <azarin.m88@gmail.com>
2026-08-03 14:52:25 +0300

    Added junit-vintage-engine

--- src/test/java/ru/netology/service/CashbackHackServiceTestJunit5.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJunit5.java
@@ -1,29 +1,29 @@
 package ru.netology.service;
 
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class CashbackHackServiceTest {
+public class CashbackHackServiceTestJunit5 {
     @Test
     public void shouldRemainWithAmountOf999() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 1;
         int actual = cashbackHackService.remain(999);
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldRemainWithAmountOf1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 0;
         int actual = cashbackHackService.remain(1000);
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldRemainWithAmountOf1001() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = 999;
         int actual = cashbackHackService.remain(1001);
-        Assert.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
